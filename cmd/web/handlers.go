b0cb64988702266f2b9a484af87431d60c0e1c00
RafaelClaumann <rafael_claumann@hotmail.com>
2024-05-11 15:02:27 -0300

    ch 9: optimize flash message load code

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -59,16 +59,8 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// Use the PopString() method to retrieve the value for the "flash" key.
-	// PopString() also deletes the key and value from the session data, so it
-	// acts like a one-time fetch. If there is no matching key in the session
-	// data this will return the empty string.
-	// Pass the flash message to the template.
-	flash := app.sessionManager.PopString(r.Context(), "flash")
-
 	data := app.newTemplateData(r)
 	data.Snippet = snippet
-	data.Flash = flash // Pass the flash message to the template
 
 	app.render(w, http.StatusOK, "view.tmpl", data)
 }
